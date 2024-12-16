bf7975fa4d523c618b12d72e6f08d31209993fcb
André Chaumet <andrechaumetcutuli@gmail.com>
2024-12-16 19:34:37 -0300

    Minor doc for PriorityNode

--- src/main/java/com/andre/limiter/PriorityNode.java
+++ src/main/java/com/andre/limiter/PriorityNode.java
@@ -1,15 +1,21 @@
 package com.andre.limiter;
 
+/**
+ * Represents a Node in a priority-based linked list. Each node tracks a priority level, a count of
+ * occurrences, and a reference to the next node in the list.
+ *
+ * <p> Designed to be used in thread-safe contexts.
+ */
 final class PriorityNode {
-  int count;
-  int priority;
+  final int priority;
   PriorityNode next;
+  int count;
 
-  PriorityNode(int priority) {
+  PriorityNode(final int priority) {
     this(priority, null);
   }
 
-  PriorityNode(int priority, PriorityNode next) {
+  PriorityNode(final int priority, PriorityNode next) {
     this.count = 1;
     this.next = next;
     this.priority = priority;
