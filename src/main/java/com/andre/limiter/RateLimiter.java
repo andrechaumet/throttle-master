5c8d599c6db7989b681ade902b9cb6e132b27000
André <andrechaumetcutuli@gmail.com>
2024-12-29 19:09:52 -0300

    Added RateLimiter interface

--- src/main/java/com/andre/limiter/RateLimiter.java
+++ src/main/java/com/andre/limiter/RateLimiter.java
@@ -2,12 +2,9 @@ package com.andre.limiter;
 
 import java.util.concurrent.TimeoutException;
 
-public interface RateLimiter {
-    void acquire() throws TimeoutException;
+public interface RateLimiter extends Limiter {
 
-    void acquire(int priority) throws TimeoutException;
+  void acquire(int priority) throws TimeoutException;
 
-    void acquire(long timeout) throws TimeoutException;
-
-    void acquire(int priority, long timeout) throws TimeoutException;
+  void acquire(int priority, long timeout) throws TimeoutException;
 }
