4646657abb55840fe5e3a32010062060f27a374a
andrechaumet <andrechaumetcutuli@gmail.com>
2025-06-07 15:25:50 -0300

    Added early version of MemoryLock feature

---
+++ src/main/java/com/andre/limiter/MemoryLock.scala
@@ -0,0 +1,75 @@
+package com.andre.limiter
+
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
+
+/**
+ * Represents a resource that can be locked.
+ *
+ * Implementations provide a unique key that identifies the resource to be locked.
+ * This key is used to ensure mutual exclusion when accessing or modifying resources
+ * of the same type.
+ */
+trait Lockable {
+
+  /**
+   * Returns the key used for locking.
+   * This key must uniquely identify the resource.
+   *
+   * @return the lock key
+   */
+  def getKey: AnyRef
+}
+
+extension (s: String)
+  def asLockable: Lockable = new Lockable {
+    def getKey: AnyRef = s
+  }
+
+extension (i: Int)
+  def asLockable: Lockable = new Lockable {
+    def getKey: AnyRef = Integer.valueOf(i)
+  }
+
+/**
+ * Locking mechanism for {@link Lockable} resources.
+ *
+ * Provides locking and unlocking for resources that implement {@code Lockable},
+ * ensuring mutual exclusion when performing operations on shared entities.
+ *
+ * @param initialCapacity initial capacity of the internal lock map
+ * @tparam T the type of lockable resource
+ */
+final class MemoryLock[T <: Lockable](initialCapacity: Int = 64) {
+
+  private val locks = new ConcurrentHashMap[AnyRef, ReentrantLock](initialCapacity)
+
+  /**
+   * Executes the given {@code Runnable} while holding the lock associated with the provided
+   * {@code lockable} key.
+   *
+   * If the lock is successfully acquired, the runnable is executed, and the lock is released
+   * afterward. If the lock cannot be acquired immediately, the runnable is not executed.
+   *
+   * @param lockable the lockable resource providing the lock key
+   * @param runnable the code to execute while holding the lock
+   */
+  def locked(lockable: T, runnable: Runnable): Unit = {
+    val key = lockable.getKey
+    if (tryAcquireLock(key)) try runnable.run() finally releaseLock(key)
+  }
+
+  private def tryAcquireLock(key: AnyRef) = {
+    val lock = locks.computeIfAbsent(key, _ => new ReentrantLock())
+    lock.tryLock()
+  }
+
+  private def releaseLock(key: AnyRef): Unit = {
+    val lock = locks.get(key)
+    if (lock != null) {
+      lock.unlock()
+      if (!lock.isLocked && !lock.hasQueuedThreads) locks.remove(key, lock)
+    }
+  }
+
+}
