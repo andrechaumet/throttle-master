27a3c121ca6b5f6e5a620e722a6ceec4a6a66c6e
andrechaumet <andrechaumetcutuli@gmail.com>
2026-03-10 20:19:35 -0300

    timeout acquire for TokenBucket

--- src/main/java/com/andre/limiter/TokenBucket.java
+++ src/main/java/com/andre/limiter/TokenBucket.java
@@ -1,10 +1,13 @@
 package com.andre.limiter;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public interface TokenBucket {
 
   void acquire() throws TimeoutException;
+
+  void acquire(long timeout, TimeUnit timeUnit) throws TimeoutException;
 
   void supply();
 }
